b598624bd0b02367de4c62008727a63c5db3ce53
Roxy <JustRoxyOsu@inbox.ru>
2022-01-31 17:08:08 +0300

    Delegate contract validation to FluentValidation

--- backend/CircleForms/Models/Posts/SubmissionContract.cs
+++ backend/CircleForms/Models/Posts/SubmissionContract.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using CircleForms.Models.Posts.Questions;
 using Newtonsoft.Json;
 
 namespace CircleForms.Models.Posts;
@@ -8,6 +9,10 @@ public class SubmissionContract
     [Required]
     [JsonProperty("question_id")]
     public int QuestionId { get; set; }
+
+    [Required]
+    [JsonProperty("question_type")]
+    public QuestionType QuestionType { get; set; }
 
     [Required]
     [JsonProperty("answer")]
