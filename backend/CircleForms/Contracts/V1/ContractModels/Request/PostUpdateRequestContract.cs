a0fa061a78947216f15427f7a468b31b4abb0f92
Roxy <JustRoxyOsu@inbox.ru>
2022-02-07 04:58:21 +0300

    Remove Accessibility from editable fields

--- backend/CircleForms/Contracts/V1/ContractModels/Request/PostUpdateRequestContract.cs
+++ backend/CircleForms/Contracts/V1/ContractModels/Request/PostUpdateRequestContract.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using CircleForms.Models;
-using CircleForms.Models.Enums;
+﻿using CircleForms.Models;
 using Newtonsoft.Json;
 
 namespace CircleForms.Contracts.V1.ContractModels.Request;
@@ -13,9 +10,6 @@ public class PostUpdateRequestContract
 
     [JsonProperty("description")]
     public string Description { get; set; }
-
-    [JsonProperty("accessibility")]
-    public Accessibility? Accessibility { get; set; }
 
     [JsonProperty("limitations")]
     public Limitations Limitations { get; set; }
