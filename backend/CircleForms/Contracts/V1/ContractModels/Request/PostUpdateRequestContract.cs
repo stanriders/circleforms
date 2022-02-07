ef2dcbe3ee738c097735710adbe2cab78fc62824
Roxy <JustRoxyOsu@inbox.ru>
2022-02-07 04:27:11 +0300

    Add PostUpdateRequestContract

---
+++ backend/CircleForms/Contracts/V1/ContractModels/Request/PostUpdateRequestContract.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using CircleForms.Models;
+using CircleForms.Models.Enums;
+using Newtonsoft.Json;
+
+namespace CircleForms.Contracts.V1.ContractModels.Request;
+
+public class PostUpdateRequestContract
+{
+    [JsonProperty("title")]
+    public string Title { get; set; }
+
+    [JsonProperty("description")]
+    public string Description { get; set; }
+
+    [JsonProperty("accessibility")]
+    public Accessibility? Accessibility { get; set; }
+
+    [JsonProperty("limitations")]
+    public Limitations Limitations { get; set; }
+}
