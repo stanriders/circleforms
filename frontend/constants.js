98e62e48386b43243842378e1ff41fad3e7bf1a2
Nemesis <keziah.moselle@gmail.com>
2022-03-29 21:27:19 +0200

    feat: improve team ui

--- frontend/constants.js
+++ frontend/constants.js
@@ -21,26 +21,46 @@ module.exports = {
       name: 'StanR',
       role: 'CEO & back-end developer',
       osuId: '7217455',
+      avatarStyles: {
+        left: '6.5rem',
+        top: '0.9rem',
+      }
     },
     {
       name: 'Arhella',
       role: 'UX/UI Designer',
       osuId: '4411044',
+      avatarStyles: {
+        right: '0',
+        top: '0',
+      }
     },
     {
       name: 'JustRoxy',
       role: 'back-end developer',
       osuId: '9083516',
+      avatarStyles: {
+        right: '5rem',
+        top: '0.8rem',
+      }
     },
     {
       name: 'kkk',
       role: 'quality assurance',
       osuId: '4735736',
+      avatarStyles: {
+        left: '5.5rem',
+        top: '0rem',
+      }
     },
     {
       name: 'Keziah',
       role: 'front-end developer',
       osuId: '5914915',
+      avatarStyles: {
+        right: '0',
+        top: '2rem'
+      }
     }
   ]
 }
