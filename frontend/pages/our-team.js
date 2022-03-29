40c09ccc5c13a466bfc5bcf4b9264d488ffdba38
Nemesis <keziah.moselle@gmail.com>
2022-03-29 22:15:42 +0200

    feat(teams): responsive

--- frontend/pages/our-team.js
+++ frontend/pages/our-team.js
@@ -18,12 +18,12 @@ export default function OurTeam() {
         <h1 className="text-6xl lg:text-8xl font-bold z-10 mt-4">OUR TEAM</h1>
       </div>
 
-      <div className="grid grid-cols-1 lg:grid-cols-4 mt-4">
+      <div className="grid grid-cols-1 mt-4 gap-y-4 mx-4 mb-4 lg:grid-cols-4 lg:gap-0 lg:m-0">
         {constants.team.map((member, index) => {
           const MemberImage = () => (
             <a
               href={`https://osu.ppy.sh/users/${member.osuId}`}
-              className="col-span-2 relative h-64 lg:h-[211px] transition hover:brightness-75 lg:col-span-1">
+              className="col-span-2 relative h-64 lg:h-[211px] transition hover:brightness-75 rounded-20 overflow-clip lg:rounded-none lg:col-span-1">
               <img
                 className="w-full h-full object-cover"
                 src={`/images/${member.name.toLowerCase()}.png`}
@@ -34,7 +34,10 @@ export default function OurTeam() {
                 src={`http://s.ppy.sh/a/${member.osuId}`}
                 alt={`${member.name} osu avatar`} />
 
-                <div className="bg-black-lightest lg:hidden absolute bottom-0 left-0 right-0 p-4 rounded-t-14">
+                <div className={classNames("bg-black-lightest lg:hidden absolute bottom-0 left-0 right-0 p-4 rounded-t-14",
+                member.name === 'StanR'
+                ? 'bg-pink text-pink-darker hover:bg-black-lightest hover:text-white'
+                : 'bg-black-lightest text-white hover:bg-pink hover:text-pink-darker')}>
                   <h2 className="font-bold text-xl lg:text-5xl ">
                     {member.name}
                   </h2>
@@ -76,7 +79,7 @@ export default function OurTeam() {
           )
         })}
 
-        <a href="https://discord.gg/rx9RKQsy9H" className="flex justify-center items-center bg-blue-discord col-span-2 text-white focus:brightness-90 hover:brightness-75 transition py-8 lg:py-0">
+        <a href="https://discord.gg/rx9RKQsy9H" className="flex justify-center items-center bg-blue-discord col-span-2 text-white focus:brightness-90 hover:brightness-75 transition py-16 rounded-20 lg:py-0 lg:rounded-none">
           <SVG className="w-32" src="/svg/discord.svg" alt="Join our Discord" />
         </a>
       </div>
