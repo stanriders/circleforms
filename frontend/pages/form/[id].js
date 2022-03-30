11d2a29fa922e77b8e61b8d5fe26e76d57bca170
Nemesis <keziah.moselle@gmail.com>
2022-03-30 22:30:27 +0200

    feat: add view route for single form

---
+++ frontend/pages/form/[id].js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import Hero from '../../components/Hero'
+import DefaultLayout from '../../layouts'
+
+export default function SingleForm() {
+  return (
+    <DefaultLayout>
+      <Head>
+        <title>CircleForms - Form</title>
+      </Head>
+    </DefaultLayout>
+  )
+}
