f2c2d2a43ff4514582df627ccd514127df337e66
JustRoxy <justroxyosu@inbox.ru>
2022-04-12 14:21:42 +0300

    Use `BsonSerializer`

--- backend/CircleForms/Contracts/ContractModels/Mappings/ContractProfile.cs
+++ backend/CircleForms/Contracts/ContractModels/Mappings/ContractProfile.cs
@@ -3,7 +3,7 @@ using CircleForms.Contracts.ContractModels.Response;
 using CircleForms.Database.Models.Posts;
 using CircleForms.Database.Models.Users;
 using Mapster;
-using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
 
 namespace CircleForms.Contracts.ContractModels.Mappings;
 
@@ -12,10 +12,10 @@ public class ContractProfile : IRegister
     public void Register(TypeAdapterConfig config)
     {
         config.NewConfig<User, UserResponseContract>()
-            .Map(x => x.Osu, x => BsonTypeMapper.MapToDotNetValue(x.Osu))
+            .Map(x => x.Osu, x => BsonSerializer.Deserialize<object>(x.Osu, null))
             .Map(x => x.ID, x => x.ID);
         config.NewConfig<User, UserAnswerContract>()
-            .Map(x => x.Osu, x => BsonTypeMapper.MapToDotNetValue(x.Osu));
+            .Map(x => x.Osu, x => BsonSerializer.Deserialize<OsuUserStatisticsContract>(x.Osu, null));
         config.ForType<Post, PostResponseContract>()
             .AfterMappingAsync(async (poco, dto) =>
             {
