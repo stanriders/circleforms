2c0ba99c7292438f04a8c6441ebe33760586ccc9
Unbound <f1arenova1@gmail.com>
2022-04-27 23:11:16 +0300

    use library error types

--- frontend/src/components/ErrorFallback.tsx
+++ frontend/src/components/ErrorFallback.tsx
@@ -1,20 +1,15 @@
 import Head from "next/head";
-import { MouseEventHandler, useEffect } from "react";
+import { FallbackProps } from "react-error-boundary";
 import DefaultLayout from "../layouts";
 
-interface IErrorFallback {
-  error: Record<string, any>;
-  resetErrorBoundary: MouseEventHandler<HTMLButtonElement>;
-}
-
-const ErrorFallback = ({ error, resetErrorBoundary }: IErrorFallback) => {
+const ErrorFallback = ({ error, resetErrorBoundary }: FallbackProps) => {
   return (
     <DefaultLayout>
       <Head>
         <title>CircleForms - An error has occured</title>
       </Head>
 
-      <div className="container text-center bg-black-light p-4 rounded-20">
+      <div role="alert" className="container text-center bg-black-light p-4 rounded-20">
         <h2 className="text-red-300 text-xl">Sorry, something went wrong:</h2>
         <pre>{error?.message}</pre>
         <button onClick={resetErrorBoundary}>Try again</button>
