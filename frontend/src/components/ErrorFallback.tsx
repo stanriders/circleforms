030b440d95b91bf6d354b6fe77dd05955d1bc96c
Unbound <f1arenova1@gmail.com>
2022-04-27 22:59:51 +0300

    add error fallback types, fix calling toString

--- frontend/src/components/ErrorFallback.tsx
+++ frontend/src/components/ErrorFallback.tsx
@@ -1,7 +1,13 @@
 import Head from "next/head";
+import { MouseEventHandler, useEffect } from "react";
 import DefaultLayout from "../layouts";
 
-export default function ErrorFallback({ error, resetErrorBoundary }) {
+interface IErrorFallback {
+  error: Record<string, any>;
+  resetErrorBoundary: MouseEventHandler<HTMLButtonElement>;
+}
+
+const ErrorFallback = ({ error, resetErrorBoundary }: IErrorFallback) => {
   return (
     <DefaultLayout>
       <Head>
@@ -10,8 +16,11 @@ export default function ErrorFallback({ error, resetErrorBoundary }) {
 
       <div className="container text-center bg-black-light p-4 rounded-20">
         <h2 className="text-red-300 text-xl">Sorry, something went wrong:</h2>
-        <pre>{error.toString()}</pre>
+        <pre>{error?.message}</pre>
+        <button onClick={resetErrorBoundary}>Try again</button>
       </div>
     </DefaultLayout>
   );
-}
+};
+
+export default ErrorFallback;
