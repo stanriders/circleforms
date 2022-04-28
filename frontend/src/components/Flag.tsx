4598ad1531ba3bf6b3d8ce5aa9a919c775d76d75
Unbound <f1arenova1@gmail.com>
2022-04-28 22:39:36 +0300

    add flag types

--- frontend/src/components/Flag.tsx
+++ frontend/src/components/Flag.tsx
@@ -1,10 +1,12 @@
 import LOCALES from "../libs/i18n";
+import { Locales } from "../types/common-types";
 
-interface ILocaleTypes {
-  flag: string;
-  locale: keyof typeof LOCALES;
+interface ILocale {
+  flag?: string;
+  locale?: Locales;
 }
-export default function Locale({ flag, locale }: ILocaleTypes) {
+
+export default function Locale({ flag, locale = "en-US" }: ILocale) {
   return (
     <span className={`fi small fi-${LOCALES[locale] ? LOCALES[locale].flag : flag} rounded-5`} />
   );
