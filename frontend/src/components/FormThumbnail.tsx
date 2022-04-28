ecfa88351d9356be8024d1154f10af2160ce2db3
Unbound <f1arenova1@gmail.com>
2022-04-28 22:40:04 +0300

    add form thumnail types

--- frontend/src/components/FormThumbnail.tsx
+++ frontend/src/components/FormThumbnail.tsx
@@ -1,25 +1,15 @@
 import Link from "next/link";
+import { PostResponse } from "../types/common-types";
 import getImage from "../utils/getImage";
 
-export default function FormThumbnail({
-  id,
-  author_id,
-  is_active,
-  icon,
-  banner,
-  title,
-  description,
-  publish_time,
-  accessibility,
-  limitations
-}) {
+export default function FormThumbnail({ id, icon, title }: PostResponse) {
   const iconImg = getImage({ icon, id, type: "icon" });
 
   return (
     <Link href={`/form/${id}`}>
       <a
         className="flex rounded-40 bg-black-lighter h-36 w-36 overflow-clip transition-all ease-out-cubic hover:rounded-14 hover:-translate-y-1"
-        title={title}
+        title={title || ""}
       >
         <img src={iconImg} alt={`${title}'s thumbnail`} />
       </a>
