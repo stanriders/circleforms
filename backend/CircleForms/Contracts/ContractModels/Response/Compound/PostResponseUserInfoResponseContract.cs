98b0d11ddf61fdf025f49136b331c94ef2f62852
JustRoxy <JustRoxyOsu@inbox.ru>
2022-04-29 01:04:27 +0300

    Rename contracts' files

--- backend/CircleForms/Contracts/ContractModels/Response/Compound/PostResponseUserInfoResponseContract.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections.Generic;
-using CircleForms.Contracts.ContractModels.Response.Compound.Abstract;
-
-namespace CircleForms.Contracts.ContractModels.Response.Compound;
-
-public class PostUserAnswerResponseContract : UserInfoResponseContract<List<UserAnswerContract>, PostResponseContract>
-{
-}
