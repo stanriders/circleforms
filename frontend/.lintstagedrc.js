f7ad74f8d8c78f3f38b4bd54199dec331a8b1155
Unbound <unbound9dev@gmail.com>
2022-05-21 23:07:52 +0300

    change lint config

--- frontend/.lintstagedrc.js
+++ frontend/.lintstagedrc.js
@@ -6,6 +6,12 @@ const buildEslintCommand = (filenames) =>
     .join(" --file ")}`;
 
 module.exports = {
-  "*.{js,jsx,ts,tsx}": [buildEslintCommand],
-  "src/**/*.{js,jsx,ts,tsx,json,css,scss,md}": ["prettier --write"]
+  // "*.{js,jsx,ts,tsx}": [buildEslintCommand],
+  // "src/**/*.{js,jsx,ts,tsx,json,css,scss,md}": ["prettier --write"]
+  "*": (files) => {
+    const isAppFolderChanges = files.some((fileName) => fileName.includes("/frontend/"));
+    const scripts = ["npm run lint", "npm run format"];
+
+    return isAppFolderChanges ? scripts : [];
+  }
 };
