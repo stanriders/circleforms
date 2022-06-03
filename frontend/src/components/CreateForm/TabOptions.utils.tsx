b41c6ccf55cec7d7661d1bbe48d7713d3e486aff
Unbound <unbound9dev@gmail.com>
2022-06-04 00:18:16 +0300

    improve form validation

--- frontend/src/components/CreateForm/TabOptions.utils.tsx
+++ frontend/src/components/CreateForm/TabOptions.utils.tsx
@@ -20,16 +20,51 @@ export const answerSchema = object({
     .default(Gamemode.None),
   questions: array()
     .of(
-      mixed<Question>().transform((currentValue) => {
-        // we need to transform questionInfo
-        // from being an array of object: [{value: string1}, {value:string2}]
-        // to just array of strings
-        const flattenedQuestions = currentValue?.questionInfo?.map(
-          (obj: Record<"value", string>) => obj.value
-        );
-        const filteredQuestionInfo = flattenedQuestions.length > 0 ? flattenedQuestions : [];
-        return { ...currentValue, questionInfo: filteredQuestionInfo };
-      })
+      mixed<Question>()
+        .transform((currentValue) => {
+          // we need to transform questionInfo
+          // from being an array of object: [{value: string1}, {value:string2}]
+          // to just array of strings
+          const flattenedQuestions = currentValue?.questionInfo?.map(
+            (obj: Record<"value", string>) => obj.value
+          );
+          const filteredQuestionInfo = flattenedQuestions.length > 0 ? flattenedQuestions : [];
+          return { ...currentValue, questionInfo: filteredQuestionInfo };
+        })
+        .test(
+          "title",
+          () => "Question title cannot be empty",
+          (value) => {
+            return Boolean(value?.title);
+          }
+        )
+        .test(
+          "checkbox-answers",
+          () => "Checkbox question must have one or more options",
+          (value) => {
+            if (value?.type === "Checkbox" && value?.questionInfo) {
+              return value?.questionInfo?.length >= 1;
+            }
+            return true;
+          }
+        )
+        .test(
+          "choice-answers",
+          () => "Radio choice question must have two or more options",
+          (value) => {
+            if (value?.type === "Choice" && value?.questionInfo) {
+              return value?.questionInfo?.length >= 2;
+            }
+            return true;
+          }
+        )
+        .test(
+          "question-info",
+          () => "Question cannot have empty options",
+          (value) => {
+            return !value?.questionInfo?.includes("")!;
+          }
+        )
     )
     .required("Create at least one question")
     .min(1, "Create at least one question")
