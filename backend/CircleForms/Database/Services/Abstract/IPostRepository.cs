edce30da8889b171dfd76c87555948cff25b9921
JustRoxy <JustRoxyOsu@inbox.ru>
2022-06-09 23:20:01 +0300

    Remove public from interface

--- backend/CircleForms/Database/Services/Abstract/IPostRepository.cs
+++ backend/CircleForms/Database/Services/Abstract/IPostRepository.cs
@@ -11,7 +11,7 @@ public interface IPostRepository
     Task<Post> Add(string userId, Post post);
     Task<List<Post>> Get();
     Task<Post> Get(string postId);
-    public Task<List<Post>> Get(List<string> posts);
+    Task<List<Post>> Get(List<string> posts);
     Task<Post> Get(string postId, Expression<Func<Post, Post>> projection);
     Task Update(Post post);
 }
