75ddcecea4be7bb8d3cca0384206a8a04041c496
Unbound <unbound9dev@gmail.com>
2022-07-29 21:23:00 +0300

    linter test error fix

--- frontend/testUtils.tsx
+++ frontend/testUtils.tsx
@@ -2,6 +2,9 @@ import { ReactElement } from "react";
 import { render, RenderOptions } from "@testing-library/react";
 import { AllTheProviders } from "src/utils/providers";
 
+import "@testing-library/jest-dom/extend-expect";
+import "@testing-library/jest-dom";
+
 const customRender = (ui: ReactElement, options?: Omit<RenderOptions, "wrapper">) =>
   render(ui, { wrapper: AllTheProviders, ...options });
 
